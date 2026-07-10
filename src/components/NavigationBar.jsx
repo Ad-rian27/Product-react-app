9fb2777fa1dfd44ae77758bd1be87f5ac325af31
Ad-rian27 <rian270627@gmail.com>
2026-07-10 11:42:15 +0530

    Added JSON properties 2

--- src/components/NavigationBar.jsx
+++ src/components/NavigationBar.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 const NavigationBar = () => {
   return (
@@ -6,16 +7,16 @@ const NavigationBar = () => {
 
         <nav class="navbar navbar-expand-lg bg-body-tertiary">
   <div class="container-fluid">
-    <a class="navbar-brand" href="#">Product App</a>
+    <Link class="navbar-brand" to="#">Product App</Link>
     <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
       <span class="navbar-toggler-icon"></span>
     </button>
     <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
       <div class="navbar-nav">
-        <a class="nav-link active" aria-current="page" href="/">Add Product</a>
-        <a class="nav-link" href="/search">Search Product</a>
-        <a class="nav-link" href="/delete">Delete Product</a>
-        <a class="nav-link" href="/view">View All Products</a>
+        <Link class="nav-link active" aria-current="page" to="/">Add Product</Link>
+        <Link class="nav-link" to="/search">Search Product</Link>
+        <Link class="nav-link" to="/delete">Delete Product</Link>
+        <Link class="nav-link" to="/view">View All Products</Link>
       </div>
     </div>
   </div>
